dd38f880c68d0bf2fd681ca402b3321a04c40ef4
rnema19 <nemarishabh9@gmail.com>
2025-03-20 19:56:26 +0530

    changed navbar

--- model/commentSchema.js
+++ model/commentSchema.js
@@ -3,10 +3,10 @@ const {Schema} = mongoose;
 
 const commentSchema = new Schema(
     {
-    comment : {type : String, required : true},
-    date : {type : Date, default : Date.now},
-    user_name : {type : String, required : true},
-    user_role : {type : String, required : true},
+    "comment" : {type : String, required : true},
+    "date" : {type : Date, default : Date.now},
+    "user_name" : {type : String, required : true},
+    "user_role" : {type : String, required : true},
     },
     {
         collection : 'comment'
