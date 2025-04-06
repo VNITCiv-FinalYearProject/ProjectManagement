b99704f942cb509838a8e0e9f1f343d6104f1662
rnema19 <nemarishabh9@gmail.com>
2025-04-06 15:52:00 +0530

    incoming commit from temp3

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,31 +1,12 @@
 const express = require("express");
-<<<<<<< HEAD
-=======
 const app = express();
->>>>>>> temp2
 const router = express.Router({ mergeParams: true });
 const authorize = require("../middleware/authorize");
-const Project = require("../model/projectSchema");
-const User = require("../model/userSchema");
 
-router.get('/',authorize(["guest","engineer", "contractor", "manager", "senior-manager", "admin"]),async(req,res)=>{
-    const projects = await Project.find({})
-    var ongoing = 0
-    var completed = 0
-    for(let i = 0;i<projects.length;i++){
-        const s_date = projects[i].startDate
-        const e_date = projects[i].expectedDate
-        var curr_date = new Date()
-        if (curr_date<e_date && curr_date>s_date) {
-            ongoing+=1
-        }
-        if (curr_date>e_date) {
-            completed+=1
-        }
-        // console.log(`Start date : ${s_date}, End date : ${e_date}`)
-    }
-
-    res.render('dashboard',{ongoing:ongoing,completed:completed,user:req.user})
-})
+// This route is protected and only accessible by users with the specified roles.
+// The middleware attaches req.user so you can use it in your template.
+router.get("/dashboard", authorize(["guest", "contractor", "manager", "senior-manager", "admin"]), (req, res) => {
+  res.render("dashboard", { user: req.user });
+});
 
 module.exports = router;
